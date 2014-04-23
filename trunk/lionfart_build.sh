45191bf19c3c90306e8a7dc99af9c20fb692e4ad
Kenneth Centurion <kcenturion@yahoo.com>
2014-04-23 16:47:39 -0500

    Create lionfart_build.sh
    Build script.

---
+++ trunk/lionfart_build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# chmod +x lionfart_build.sh
+#
+# This script will get latest site from github, then stop
+# apache and jboss and restart.
+#
+
+SRC_DIR="/usr/src/lf"
+
+service apache2 stop
+service jbossas7 stop
+
+if [ ! -d "/usr/src/lf" ]; then
+    echo "Creating /usr/src/lf dir"
+    mkdir /usr/src/lf
+fi
+
+#Initially get the repo.
+#git clone https://github.com/zoroloco/lionfart.git /usr/src/lionfart
+
+cd $SRC_DIR
+
+git pull
+
+cd $SRC_DIR/trunk
+
+mvn clean
+
+mvn install
+
+echo "Now deploying EAR file and standalone.xml files."
+
+cp $SRC_DIR/trunk/standalone.xml /usr/jboss/standalone/configuration/
+cp -R $SRC_DIR/trunk/LionFartEar/target/lionfartear-1.0.0-RC1.ear /usr/jboss/standalone/deployments/
+
+service jbossas7 start
+service apache2 start
