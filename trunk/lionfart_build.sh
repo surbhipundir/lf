2fcf4bda3532d2adbe748859a581222b76088b6e
Kenneth Centurion <kcenturion@yahoo.com>
2014-10-11 11:39:20 -0500

    Update lionfart_build.sh

--- trunk/lionfart_build.sh
+++ trunk/lionfart_build.sh
@@ -46,7 +46,12 @@ mvn install package
 #cp $SRC_DIR/trunk/.m2/settings.xml /root/.m2/
 
 echo "Now deploying JAR/WAR/EAR file(s) to JBOSS_HOME"
-rm -f $JBOSS_DIR/standalone/deployments/*.jar
+rm -f $JBOSS_DIR/standalone/deployments/lionfart.war
+touch $JBOSS_DIR/standalone/deployments/lionfart.war.dodeploy
+
+chown jboss $JBOSS_DIR/standalone/deployments/lionfart.war.deploy
+chgrp jboss $JBOSS_DIR/standalone/deployments/lionfart.war.deploy
+
 cp -R $SRC_DIR/trunk/lionfart/target/lionfart.war $JBOSS_DIR/standalone/deployments
 
 echo "Now lets start up jboss and apache again and hope this all works!"
