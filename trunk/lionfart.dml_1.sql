849b780cbb08d6eefa73c26618d03be0aeb3263f
Kenneth Centurion <kcenturion@yahoo.com>
2014-10-15 15:02:01 -0500

    Update lionfart.dml_1.sql

--- trunk/lionfart.dml_1.sql
+++ trunk/lionfart.dml_1.sql
@@ -8,3 +8,6 @@ insert into `role`(`ROLE_ID`,`NAME`,`CREATED_BY`,`CREATED_TIME`,`MODIFIED_BY`,`M
 insert into `user`(`USER_ID`,`USER_NAME`,`PASSWORD`,`EMAIL`,`FIRST_NAME`,`LAST_NAME`,`CREATED_BY`,`CREATED_TIME`,`MODIFIED_BY`,`MODIFIED_TIME`) 
   values (2,'kcenturion','SET YOUR PASSWORD HERE',null,null,null,'kcenturion',NOW(),null,null);
 
+
+insert into `user_role`(`USER_ROLE_ID`,`USER_ID`,`ROLE_ID`,`CREATED_BY`,`CREATED_TIME`,`MODIFIED_BY`,`MODIFIED_TIME`) 
+  values (2,2,1,'kcenturion',NOW(),null,null);
