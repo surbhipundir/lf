6d9400f701647cc3535e1142b934b64969c5d150
Kenneth Centurion <kcenturion@yahoo.com>
2014-10-15 11:10:34 -0500

    Create lionfart.dml_1.sql

---
+++ trunk/lionfart.dml_1.sql
@@ -0,0 +1,6 @@
+
+insert into `role`(`ROLE_ID`,`NAME`,`CREATED_BY`,`CREATED_TIME`,`MODIFIED_BY`,`MODIFIED_TIME`) 
+  values (1,'Administrator','kcenturion',NOW(),null,NOW());
+  
+insert into `role`(`ROLE_ID`,`NAME`,`CREATED_BY`,`CREATED_TIME`,`MODIFIED_BY`,`MODIFIED_TIME`) 
+  values (2,'User','kcenturion',NOW(),null,NOW());
