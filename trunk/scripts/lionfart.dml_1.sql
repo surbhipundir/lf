073dbdc91ea2fe454de385fe4d15a0194d8c5e98
Kenneth Centurion <kcenturion@yahoo.com>
2014-11-05 14:12:10 -0600

    Update lionfart.dml_1.sql

--- trunk/scripts/lionfart.dml_1.sql
+++ trunk/scripts/lionfart.dml_1.sql
@@ -1,13 +1,15 @@
+delete from user_role;
+delete from user;
+delete from role;
 
-insert into `role`(`ROLE_ID`,`NAME`,`CREATED_BY`,`CREATED_TIME`,`MODIFIED_BY`,`MODIFIED_TIME`) 
+insert into `role`(`ID`,`NAME`,`CREATED_BY`,`CREATED_DATE`,`MODIFIED_BY`,`MODIFIED_DATE`) 
   values (1,'Administrator','kcenturion',NOW(),null,NOW());
   
-insert into `role`(`ROLE_ID`,`NAME`,`CREATED_BY`,`CREATED_TIME`,`MODIFIED_BY`,`MODIFIED_TIME`) 
+insert into `role`(`ID`,`NAME`,`CREATED_BY`,`CREATED_DATE`,`MODIFIED_BY`,`MODIFIED_DATE`) 
   values (2,'User','kcenturion',NOW(),null,NOW());
   
-insert into `user`(`USER_ID`,`USER_NAME`,`PASSWORD`,`EMAIL`,`FIRST_NAME`,`LAST_NAME`,`CREATED_BY`,`CREATED_TIME`,`MODIFIED_BY`,`MODIFIED_TIME`) 
-  values (2,'kcenturion','SET YOUR PASSWORD HERE',null,null,null,'kcenturion',NOW(),null,null);
+insert into `user`(`ID`,`USER_NAME`,`PASSWORD`,`EMAIL`,`FIRST_NAME`,`LAST_NAME`,`CREATED_BY`,`CREATED_DATE`,`MODIFIED_BY`,`MODIFIED_DATE`) 
+  values (2,'test','test',null,null,null,'kcenturion',NOW(),null,null);
 
-
-insert into `user_role`(`USER_ROLE_ID`,`USER_ID`,`ROLE_ID`,`CREATED_BY`,`CREATED_TIME`,`MODIFIED_BY`,`MODIFIED_TIME`) 
+insert into `user_role`(`ID`,`USER_ID`,`ROLE_ID`,`CREATED_BY`,`CREATED_DATE`,`MODIFIED_BY`,`MODIFIED_DATE`) 
   values (2,2,1,'kcenturion',NOW(),null,null);
